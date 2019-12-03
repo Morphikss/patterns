cdba59c7f08487ca5ea81c846e9621ef8b4da5d3
Morphikss <44949530+Morphikss@users.noreply.github.com>
2019-12-03 21:57:22 +0100

    facade

--- facade/src/test/java/FacadeTest.java
+++ facade/src/test/java/FacadeTest.java
@@ -1,6 +1,15 @@
 import static org.junit.Assert.*;
 
 public class FacadeTest {
-    
+    @org.junit.Test
+    public void test() {
+        MainApi api = new MainApi(400,200);
+        api.tranfer(200);
+        assertEquals(api.getSenderBalance(),api.getReceiverBalance());
+        System.out.println(api.getSenderBalance());
+        System.out.println(api.getReceiverBalance());
+
+        
+    }
 
 }
