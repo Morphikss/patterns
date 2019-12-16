b4d15df92bdd774692524280bc9c1497be2db207
Morphikss <44949530+Morphikss@users.noreply.github.com>
2019-12-16 08:59:23 +0100

    iterator

--- iterator/src/main/java/Main.java
+++ iterator/src/main/java/Main.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static List<User> users = new ArrayList<User>();
+    public static void main( String[] args ) {
+        users.add(new User("Janusz", "Janusz1"));
+        users.add(new User("Andrzej", "Andrzej1"));
+        users.add(new User("Jan", "Jan1"));
+        users.add(new User("Tomasz", "Tomasz1"));
+
+        printUsers(new UserListIterator(users));
+    }
+
+    public static void printUsers(Iterator iterator){
+        if(iterator != null){
+            while(iterator.hasNext()) System.out.println(iterator.next());
+        }
+    }
+
 }
