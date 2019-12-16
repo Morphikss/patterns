b7409cb67a097571c1b0583c5e867b133d9f2c31
Morphikss <44949530+Morphikss@users.noreply.github.com>
2019-12-16 19:27:45 +0100

    iterator

--- iterator/src/main/java/Main.java
+++ iterator/src/main/java/Main.java
@@ -1,21 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
 public class Main {
-    static List<User> users = new ArrayList<User>();
+
     public static void main( String[] args ) {
-        users.add(new User("Janusz", "Janusz1"));
-        users.add(new User("Andrzej", "Andrzej1"));
-        users.add(new User("Jan", "Jan1"));
-        users.add(new User("Tomasz", "Tomasz1"));
+        PizzaMenuApi menuApi = new PizzaMenuApi();
 
-        printUsers(new UserListIterator(users));
-    }
+        menuApi.addPizza(new Pizza("Capriciosa", 2));
+        menuApi.addPizza(new Pizza("Chicken", 3));
+        menuApi.addPizza(new Pizza("Hawaii", 4));
 
-    public static void printUsers(Iterator iterator){
-        if(iterator != null){
-            while(iterator.hasNext()) System.out.println(iterator.next());
-        }
+        Iterator<Pizza> iterator = menuApi.iterator();
+        menuApi.printPizzas(iterator);
     }
 
 }
